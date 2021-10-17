e5d71a4c86544c72c4d9c3f863daa2ea73667e10
Ivan Stosic <ivan100sic@gmail.com>
2021-10-17 23:42:28 +0200

    Added bmain

---
+++ main/bmain.11.cpp
@@ -0,0 +1,20 @@
+// Basic program skeleton
+
+#define SNIPPET_ARG(x)
+
+/*snippet-begin*/
+// Retired?
+// Retired?
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = long unsigned long;
+using ld = double long;
+
+int main() {
+    ios::sync_with_stdio(!cin.tie(0));
+
+    SNIPPET_ARG(0)
+}
+/*snippet-end*/
