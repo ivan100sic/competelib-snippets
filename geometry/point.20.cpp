f4f0ab6288a6a63f493a3f4c48bd42f44ee15df6
Ivan Stosic <ivan100sic@gmail.com>
2021-10-18 15:29:35 +0200

    Fixed C++20 comparison in geometry/point

--- geometry/point.20.cpp
+++ geometry/point.20.cpp
@@ -21,6 +21,7 @@ struct point {
     point operator- (const point& b) const { return {x-b.x, y-b.y}; }
     point& operator-= (const point& b) { x -= b.x; y -= b.y; return *this; }
     strong_ordering operator<=> (const point& b) const = default;
+    bool operator== (const point& b) const = default;
     template<class R = ll> R sp(const point& b) const { return (R)x*b.x + (R)y*b.y; }
     template<class R = ll> R vp(const point& b) const { return (R)x*b.y - (R)y*b.x; }
 };
@@ -31,5 +32,5 @@ using ptll = point<ll>;
 
 int main() {
     pti a = {1, 2}, b = {2, 5};
-    return a.sp(b) != 12 || a.vp(b) != 1 || a >= b;
+    return a.sp(b) != 12 || a.vp(b) != 1 || a == b || !(a != b);
 }
