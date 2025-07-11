a851bae04f77ed9155be4cb50d0f0e6a9c2de400
Ivan Stosic <ivan100sic@gmail.com>
2025-07-11 14:35:34 +0200

    Add version snippet, modify pipeline

---
+++ basic/version.3.cpp
@@ -0,0 +1,9 @@
+// Prints the version information of competelib-snippets.
+
+/*snippet-begin*/
+// Release version: /*snippet-release-version*/
+// Release date: /*snippet-release-date*/
+// Project homepage: https://github.com/ivan100sic/competelib-snippets
+/*snippet-end*/
+
+int main() {}
